d6df7365778ba845843d632b8e6be8a4ab95892b
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-10-21 00:41:21 +0100

    feat: Map and test events

--- send_events.sh
+++ send_events.sh
@@ -2,5 +2,20 @@
 
 set -e
 
-curl -X POST -H "content-type: application/json" -H "X-GitHub-Event: issues" -d @sample/issue.json \
+curl -H "content-type: application/json" -H "X-GitHub-Event: issues" -d @sample/issue.json \
+  http://localhost:8065/plugins/org.holochain.mm-plugin/github
+
+curl -H "content-type: application/json" -H "X-GitHub-Event: pull_request" -d @sample/pull_request_1.json \
+  http://localhost:8065/plugins/org.holochain.mm-plugin/github
+
+curl -H "content-type: application/json" -H "X-GitHub-Event: pull_request" -d @sample/pull_request_2.json \
+  http://localhost:8065/plugins/org.holochain.mm-plugin/github
+
+curl -H "content-type: application/json" -H "X-GitHub-Event: pull_request" -d @sample/pull_request_closed_2.json \
+  http://localhost:8065/plugins/org.holochain.mm-plugin/github
+
+curl -H "content-type: application/json" -H "X-GitHub-Event: release" -d @sample/prerelease.json \
+  http://localhost:8065/plugins/org.holochain.mm-plugin/github
+
+curl -H "content-type: application/json" -H "X-GitHub-Event: release" -d @sample/release.json \
   http://localhost:8065/plugins/org.holochain.mm-plugin/github
--- server/github.go
+++ server/github.go
@@ -19,24 +19,131 @@ func (p *Plugin) startGithubEventListener() {
 
 	teamName := strings.TrimSpace(config.MattermostTeamName)
 	issueFeed := strings.TrimSpace(config.MattermostIssueFeedChannelName)
+	prFeed := strings.TrimSpace(config.MattermostPullRequestChannelName)
+	releaseFeed := strings.TrimSpace(config.MattermostReleaseCreatedChannelName)
 
 	if teamName != "" && issueFeed != "" {
 		eventHandler.OnIssuesEventOpened(
 			func(ctx context.Context, deliveryID string, eventName string, event *github.IssuesEvent) error {
-				// Skip draft issues
-				if event.GetIssue().GetDraft() {
-					return nil
-				}
+				issue := event.GetIssue()
 
 				// Skip reopened issues
-				if event.GetIssue().GetStateReason() == "reopened" {
+				if issue.GetStateReason() == "reopened" {
 					return nil
 				}
 
-				return p.sendNewIssueMessage(event.GetIssue(), teamName, issueFeed)
+				return p.sendNewIssueMessage(issue, teamName, issueFeed)
 			})
 	} else {
 		println("Mattermost team name or issue feed channel name is not set, skipping issue event listener setup")
+	}
+
+	if teamName != "" && prFeed != "" {
+		eventHandler.OnPullRequestEventOpened(
+			func(ctx context.Context, deliveryID string, eventName string, event *github.PullRequestEvent) error {
+				repo := event.GetRepo()
+				pullRequest := event.GetPullRequest()
+
+				// Skip draft pull requests
+				if pullRequest.GetDraft() {
+					return nil
+				}
+
+				term := fmt.Sprintf("#%s.%s.%d", repo.GetOwner().GetName(), repo.GetName(), pullRequest.GetNumber())
+				posts, err := p.findPostsByTerm(term, teamName, prFeed)
+				if err != nil {
+					return fmt.Errorf("failed to find posts by term %s: %w", term, err)
+				}
+
+				if len(posts) > 0 {
+					// Ensure that the post is pinned
+					for _, post := range posts {
+						if !post.IsPinned {
+							post.IsPinned = true
+							err = p.client.Post.UpdatePost(post)
+							if err != nil {
+								return fmt.Errorf("failed to update post in channel %s: %w", prFeed, err)
+							}
+						}
+					}
+
+					// Pull request message already exists, do not send a duplicate
+					return nil
+				}
+
+				return p.sendMessage(
+					fmt.Sprintf("#%s.%s.%d %s\n\n%s", repo.GetOwner().GetName(), repo.GetName(), pullRequest.GetNumber(), pullRequest.GetTitle(), pullRequest.GetHTMLURL()),
+					teamName,
+					prFeed, true)
+			})
+
+		eventHandler.OnPullRequestEventReadyForReview(
+			func(ctx context.Context, deliveryID string, eventName string, event *github.PullRequestEvent) error {
+				repo := event.GetRepo()
+				pullRequest := event.GetPullRequest()
+
+				term := fmt.Sprintf("#%s.%s.%d", repo.GetOwner().GetName(), repo.GetName(), pullRequest.GetNumber())
+				posts, err := p.findPostsByTerm(term, teamName, prFeed)
+				if err != nil {
+					return fmt.Errorf("failed to find posts by term %s: %w", term, err)
+				}
+
+				if len(posts) > 0 {
+					// Ensure that the post is pinned
+					for _, post := range posts {
+						if !post.IsPinned {
+							post.IsPinned = true
+							err = p.client.Post.UpdatePost(post)
+							if err != nil {
+								return fmt.Errorf("failed to update post in channel %s: %w", prFeed, err)
+							}
+						}
+					}
+
+					// Pull request message already exists, do not send a duplicate
+					return nil
+				}
+
+				return p.sendMessage(
+					fmt.Sprintf("#%s.%s.%d %s\n\n%s", repo.GetOwner().GetName(), repo.GetName(), pullRequest.GetNumber(), pullRequest.GetTitle(), pullRequest.GetHTMLURL()),
+					teamName,
+					prFeed, true)
+			})
+
+		eventHandler.OnPullRequestEventClosed(
+			func(ctx context.Context, deliveryID string, eventName string, event *github.PullRequestEvent) error {
+				repo := event.GetRepo()
+				pullRequest := event.GetPullRequest()
+				term := fmt.Sprintf("#%s.%s.%d", repo.GetOwner().GetName(), repo.GetName(), pullRequest.GetNumber())
+
+				return p.unpinMessages(term, teamName, prFeed)
+			})
+	} else {
+		println("Mattermost team name or pull request feed channel name is not set, skipping pull request event listener setup")
+	}
+
+	if teamName != "" && releaseFeed != "" {
+		eventHandler.OnReleaseEventReleased(
+			func(ctx context.Context, deliveryID string, eventName string, event *github.ReleaseEvent) error {
+				release := event.GetRelease()
+
+				return p.sendMessage(
+					fmt.Sprintf("%s - %s\n\n%s", release.GetName(), release.GetTagName(), release.GetHTMLURL()),
+					teamName,
+					releaseFeed, false)
+			})
+
+		eventHandler.OnReleaseEventPreReleased(
+			func(ctx context.Context, deliveryID string, eventName string, event *github.ReleaseEvent) error {
+				release := event.GetRelease()
+
+				return p.sendMessage(
+					fmt.Sprintf("Pre-release: %s - %s\n\n%s", release.GetName(), release.GetTagName(), release.GetHTMLURL()),
+					teamName,
+					releaseFeed, false)
+			})
+	} else {
+		println("Mattermost team name or release feed channel name is not set, skipping release event listener setup")
 	}
 
 	p.eventHandler = eventHandler
@@ -46,45 +153,18 @@ func (p *Plugin) sendNewIssueMessage(issue *github.Issue, teamName, issueFeed st
 	return p.sendMessage(
 		fmt.Sprintf("%s\n\n%s", issue.GetTitle(), issue.GetHTMLURL()),
 		teamName,
-		issueFeed)
+		issueFeed, false)
 }
 
-func (p *Plugin) sendMessage(message, teamName, channelName string) error {
+func (p *Plugin) sendMessage(message, teamName, channelName string, pinned bool) error {
 	botUserId := p.botUserId
 	if botUserId == nil {
 		return fmt.Errorf("bot user ID is nil")
 	}
 
-	team, err := p.client.Team.GetByName(teamName)
+	team, err := p.ensureTeam(*botUserId, teamName)
 	if err != nil {
-		return fmt.Errorf("failed to get team by name %s: %w", teamName, err)
-	}
-
-	foundBotUserInTeam := false
-	page := 0
-out:
-	for {
-		memberList, err := p.client.Team.ListMembers(team.Id, page, 100)
-		if err != nil {
-			return fmt.Errorf("failed to list team members: %w", err)
-		}
-
-		if len(memberList) < 100 {
-			break
-		}
-
-		for _, member := range memberList {
-			if member.UserId == *botUserId {
-				foundBotUserInTeam = true
-				break out
-			}
-		}
-
-		page += 1
-	}
-
-	if !foundBotUserInTeam {
-		_, err = p.client.Team.CreateMember(team.Id, *botUserId)
+		return fmt.Errorf("failed to ensure team %s: %w", teamName, err)
 	}
 
 	channel, err := p.client.Channel.GetByName(team.Id, channelName, false)
@@ -93,7 +173,7 @@ out:
 	}
 
 	err = p.client.Post.CreatePost(&model.Post{
-		IsPinned:  true,
+		IsPinned:  pinned,
 		UserId:    *botUserId,
 		ChannelId: channel.Id,
 		Message:   message,
@@ -103,6 +183,95 @@ out:
 	}
 
 	return nil
+}
+
+func (p *Plugin) unpinMessages(term, teamName, channelName string) error {
+	posts, err := p.findPostsByTerm(term, teamName, channelName)
+	if err != nil {
+		return fmt.Errorf("failed to find posts by term %s: %w", term, err)
+	}
+
+	for _, post := range posts {
+		if post.IsPinned {
+			post.IsPinned = false
+			err = p.client.Post.UpdatePost(post)
+			if err != nil {
+				return fmt.Errorf("failed to update post in channel %s: %w", channelName, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (p *Plugin) findPostsByTerm(term, teamName, channelName string) ([]*model.Post, error) {
+	botUserId := p.botUserId
+	if botUserId == nil {
+		return nil, fmt.Errorf("bot user ID is nil")
+	}
+
+	team, err := p.ensureTeam(*botUserId, teamName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to ensure team %s: %w", teamName, err)
+	}
+
+	channel, err := p.client.Channel.GetByName(team.Id, channelName, false)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get channel by name %s: %w", channelName, err)
+	}
+
+	posts, err := p.client.Post.SearchPostsInTeam(team.Id, []*model.SearchParams{{
+		Terms: term,
+	}})
+	if err != nil {
+		return nil, fmt.Errorf("failed to search posts in team %s: %w", teamName, err)
+	}
+
+	// Filter posts to only those in the specified channel
+	var filteredPosts []*model.Post
+	for _, post := range posts {
+		if post.ChannelId == channel.Id && post.UserId == *botUserId {
+			filteredPosts = append(filteredPosts, post)
+		}
+	}
+
+	return filteredPosts, nil
+}
+
+func (p *Plugin) ensureTeam(botUserId, teamName string) (*model.Team, error) {
+	team, err := p.client.Team.GetByName(teamName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get team by name %s: %w", teamName, err)
+	}
+
+	foundBotUserInTeam := false
+	page := 0
+out:
+	for {
+		memberList, err := p.client.Team.ListMembers(team.Id, page, 100)
+		if err != nil {
+			return nil, fmt.Errorf("failed to list team members: %w", err)
+		}
+
+		if len(memberList) < 100 {
+			break
+		}
+
+		for _, member := range memberList {
+			if member.UserId == botUserId {
+				foundBotUserInTeam = true
+				break out
+			}
+		}
+
+		page += 1
+	}
+
+	if !foundBotUserInTeam {
+		_, err = p.client.Team.CreateMember(team.Id, botUserId)
+	}
+
+	return team, nil
 }
 
 // ServerHTTP handles HTTP requests made to the plugin.
